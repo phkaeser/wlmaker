a3e2b809b78987e40879dd940870ef163842e507
Philipp Kaeser <kaeser@gubbe.ch>
2023-08-11 17:46:31 +0200

    Adds thoughts on object model.

--- src/toolkit/toolkit.h
+++ src/toolkit/toolkit.h
@@ -24,6 +24,7 @@
  * Work in progress ...
  *
  * * Where do we compose, vs. inherit?
+ * * How do we map tiles in a dock, or at the clip?
  *
  * @startuml
 
@@ -32,13 +33,16 @@
      struct wlr_scene_node *node_ptr
      Container *container_ptr
 
-     Element *create(void)
+     init(handlers)
      void destroy(Element*)
      set_parent(Container*)
      map()
      unmap()
 
-     {abstract}#create_node()
+     {abstract}#void enter(Element*)
+     {abstract}#void leave(Element*)
+     {abstract}#void click(Element*)
+     {abstract}#create_or_reparent_node(Element*, parent_node*)
    }
    note right of Element::"map()"
      Only permitted if element is a member of a (mapped?) container.
@@ -53,18 +57,26 @@
      add_element(Element*)
      remove_element(Element*)
 
-     map()
-     unmap()
+     -void enter(Element*)
+     -void leave(Element*)
+     -void click(Element*)
    }
    Element <|-- Container
 
    class Workspace {
      Container parent
+     Container layers[]
 
      Container *create(void)
      void destroy(Container*)
+
+     map_window(Window*)
+     unmap_window(Window*)
+
+     map_layer_element(LayerElement *, layer)
+     unmap_layer_element(LayerElement *, layer)
    }
-   Container <|-- Workspace
+   Container *-- Workspace
 
    class HBox {
      Container parent
@@ -76,41 +88,157 @@
    }
    Container <|-- VBox
 
-   class Surface {
+   abstract class Content {
      Element parent
+
+     init(handlers)
+
+     {abstract}#void set_active(bool)
+     {abstract}#void set_maximized(bool)
+     {abstract}#void set_fullscreen(bool)
+     {abstract}#void enter(Element*)
+     {abstract}#void leave(Element*)
+     {abstract}#void click(Element*)
    }
-   Element <|-- Surface
+   Element <|-- Content
+   note right of Content
+     Interface for Window contents.
+     A surface (or... buffer? ...). Ultimately wraps a node,
+     thus may be an element.
+   end note
+
+   class LayerElement {
+     Element parent
+
+     {abstract}#void enter(Element*)
+     {abstract}#void leave(Element*)
+     {abstract}#void click(Element*)
+
+     {abstract}#configure()
+     }
+   Element <|-- LayerElement
+
+   class LayerShell {
+   }
+   LayerElement <|-- LayerShell
+
+   class XdgToplevelSurface {
+   }
+   Content <|-- XdgToplevelSurface
 
    class Buffer {
      Element parent
    }
    Element <|-- Buffer
 
+   class Button {
 
-   class Workspace {
-     Container parent
-
-     Workspace *create(void)
-     void destroy(Workspace*)
    }
-
-
+   Buffer <|-- Button
 
    class Window {
      VBox parent
-     Surface surface
+     Content content
      TitleBar title_bar
    }
-   VBox <|-- Window
+   VBox *-- Window
 
    class TitleBar {
      HBox parent
    }
-   HBox <|-- TitleBar
+   HBox *-- TitleBar
 
+   class Menu {
+     VBox parent
+   }
+   VBox *-- Menu
+
+   class MenuItem {
+
+   }
+   Buffer <|-- MenuItem
 
  * @enduml
+ *
+ *
  */
+
+#if 0
+// Scenario: Create a window
+
+xdg_toplevel... => on handle_new_surface
+
+* XdgToplevelSurface::create(wlr surface)
+  * listeners for map, unmap, destroy
+
+  => so yes, what will this do when mapped?
+
+* Window::create(surface)
+  * registers the window for workspace
+
+  * creates the container, with parent of window::element
+  * if decoration:
+
+
+
+
+* will setup listeners for the various events, ...
+  * request maximize
+  * request move
+  * request show window menu
+  * set title
+  * ...
+
+
+
+set title handler:
+* window::set_title
+
+request maximize handler:
+* window::request_maximize
+  * window::set_maximized
+    * internally: get view from workspace, ... set_size
+    * callback to surface (if set): set_maximized
+
+
+upon surface::map
+
+* workspace::add_window(window)   (unsure: do we need this?)
+  => should set "container" of window::parent...::element to workspace::container
+    (ie. set_parent(...); and add "element" to "container")
+
+* workspace::map_window(window)
+  => this should add window to the set of workspace::mapped_windows
+  => window::element->container -> map_element(element)
+    (expects the container to be mapped)
+
+  => will call map(node?) on window::element
+    - is implemented in Container:
+    - create a scene tree (from parent's node) oc reparent (from parent)
+    - calls map for every item in container
+
+
+upon surface::unmap
+* workspace::unmap_window
+
+  => window::element->container -> unmap_element(element)
+  => will call unmap() on window::element
+     => destroy the node
+
+* workspace::remove_window(window)   (do we need this?)
+
+
+
+
+There is a click ("pointer button event") -> goes to workspace.
+
+* use node lookup -> should give data -> element
+* element::click(...)
+* Button::click gets called. Has a "button_from_element" & goes from there.
+
+#endif
+
+
 #ifndef __TOOLKIT_H__
 #define __TOOLKIT_H__
 
