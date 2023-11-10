c669b0189c58f6c9380eb7e959ea512671b3a547
Philipp Kaeser <kaeser@gubbe.ch>
2023-11-10 22:08:07 +0100

    Adds initial implementation of toolkit titlebar buttons.

--- src/toolkit/titlebar.c
+++ src/toolkit/titlebar.c
@@ -21,6 +21,7 @@
 #include "titlebar.h"
 
 #include "box.h"
+#include "button.h"
 #include "buffer.h"
 #include "gfxbuf.h"
 #include "primitives.h"
@@ -33,6 +34,8 @@
 
 /** Forward declaration. */
 typedef struct _wlmtk_titlebar_title_t wlmtk_titlebar_title_t;
+/** Forward declaration. */
+typedef struct _wlmtk_titlebar_button_t wlmtk_titlebar_button_t;
 
 /** State of the title bar. */
 struct _wlmtk_titlebar_t {
@@ -41,6 +44,9 @@ struct _wlmtk_titlebar_t {
 
     /** Title element of the title bar. */
     wlmtk_titlebar_title_t    *title_ptr;
+
+    /** Minimize button. */
+    wlmtk_titlebar_button_t  *minimize_button_ptr;
 
     /** Titlebar background, when focussed. */
     bs_gfxbuf_t               *focussed_gfxbuf_ptr;
@@ -83,6 +89,39 @@ static bool wlmtk_titlebar_title_redraw(
     int width,
     bool activated,
     const wlmtk_titlebar_style_t *style_ptr);
+
+/** Function pointer to method for drawing the button contents. */
+typedef void (*wlmtk_titlebar_button_draw_t)(
+    cairo_t *cairo_ptr, uint32_t color);
+
+/** State of a titlebar button. */
+struct _wlmtk_titlebar_button_t {
+    /** Superclass: Button. */
+    wlmtk_button_t            super_button;
+
+    /** For drawing the button contents. */
+    wlmtk_titlebar_button_draw_t draw;
+
+    /** WLR buffer of the button when focussed & released. */
+    struct wlr_buffer         *focussed_released_wlr_buffer_ptr;
+    /** WLR buffer of the button when focussed & pressed. */
+    struct wlr_buffer         *focussed_pressed_wlr_buffer_ptr;
+    /** WLR buffer of the button when blurred. */
+    struct wlr_buffer         *blurred_wlr_buffer_ptr;
+};
+
+static wlmtk_titlebar_button_t *wlmtk_titlebar_button_create(
+    wlmtk_titlebar_button_draw_t draw);
+static void wlmtk_titlebar_button_destroy(
+    wlmtk_titlebar_button_t *titlebar_button_ptr);
+static bool wlmtk_titlebar_button_redraw(
+    wlmtk_titlebar_button_t *titlebar_button_ptr,
+    bs_gfxbuf_t *focussed_gfxbuf_ptr,
+    bs_gfxbuf_t *blurred_gfxbuf_ptr,
+    int position,
+    const wlmtk_titlebar_style_t *style_ptr);
+wlmtk_element_t *wlmtk_titlebar_button_element(
+    wlmtk_titlebar_button_t *titlebar_button_ptr);
 
 static void titlebar_box_destroy(wlmtk_box_t *box_ptr);
 static bool redraw_buffers(
@@ -154,12 +193,33 @@ wlmtk_titlebar_t *wlmtk_titlebar_create(
         &titlebar_ptr->super_box.super_container,
         &titlebar_ptr->title_ptr->super_buffer.super_element);
 
+    titlebar_ptr->minimize_button_ptr = wlmtk_titlebar_button_create(
+        wlmaker_primitives_draw_minimize_icon);
+    if (NULL == titlebar_ptr->minimize_button_ptr) {
+        wlmtk_titlebar_destroy(titlebar_ptr);
+        return NULL;
+    }
+    wlmtk_element_set_visible(
+        wlmtk_titlebar_button_element(titlebar_ptr->minimize_button_ptr),
+        true);
+    wlmtk_container_add_element(
+        &titlebar_ptr->super_box.super_container,
+        wlmtk_titlebar_button_element(titlebar_ptr->minimize_button_ptr));
+
     return titlebar_ptr;
 }
 
 /* ------------------------------------------------------------------------- */
 void wlmtk_titlebar_destroy(wlmtk_titlebar_t *titlebar_ptr)
 {
+    if (NULL != titlebar_ptr->minimize_button_ptr) {
+        wlmtk_container_remove_element(
+            &titlebar_ptr->super_box.super_container,
+            wlmtk_titlebar_button_element(titlebar_ptr->minimize_button_ptr));
+        wlmtk_titlebar_button_destroy(titlebar_ptr->minimize_button_ptr);
+        titlebar_ptr->minimize_button_ptr = NULL;
+    }
+
     if (NULL != titlebar_ptr->title_ptr) {
         wlmtk_container_remove_element(
             &titlebar_ptr->super_box.super_container,
@@ -198,6 +258,15 @@ bool wlmtk_titlebar_set_width(
             0,
             titlebar_ptr->width,
             titlebar_ptr->activated,
+            &titlebar_ptr->style)) {
+        return false;
+    }
+
+    if (!wlmtk_titlebar_button_redraw(
+            titlebar_ptr->minimize_button_ptr,
+            titlebar_ptr->focussed_gfxbuf_ptr,
+            titlebar_ptr->blurred_gfxbuf_ptr,
+            0,
             &titlebar_ptr->style)) {
         return false;
     }
@@ -484,6 +553,184 @@ bool title_redraw_buffers(
     return true;
 }
 
+/* == Title bar button ===================================================== */
+
+static void titlebar_button_destroy(wlmtk_button_t *button_ptr);
+static struct wlr_buffer *create_buf(
+    bs_gfxbuf_t *gfxbuf_ptr,
+    int position,
+    bool pressed,
+    const wlmtk_titlebar_style_t *style_ptr,
+    wlmtk_titlebar_button_draw_t draw);
+
+/** Buffer implementation for title of the title bar. */
+static const wlmtk_button_impl_t titlebar_button_impl = {
+    .destroy = titlebar_button_destroy
+};
+
+/* ------------------------------------------------------------------------- */
+/**
+ * Creates a button for the titlebar.
+ *
+ * @param draw
+ *
+ * @return Pointer to the titlebar button, or NULL on error.
+ */
+wlmtk_titlebar_button_t *wlmtk_titlebar_button_create(
+    wlmtk_titlebar_button_draw_t draw)
+{
+    wlmtk_titlebar_button_t *titlebar_button_ptr = logged_calloc(
+        1, sizeof(wlmtk_titlebar_button_t));
+    if (NULL == titlebar_button_ptr) return NULL;
+    titlebar_button_ptr->draw = draw;
+
+    if (!wlmtk_button_init(
+            &titlebar_button_ptr->super_button,
+            &titlebar_button_impl)) {
+        wlmtk_titlebar_button_destroy(titlebar_button_ptr);
+        return NULL;
+    }
+
+    return titlebar_button_ptr;
+}
+
+/* ------------------------------------------------------------------------- */
+/**
+ * Destroys the titlebar button.
+ *
+ * @param titlebar_button_ptr
+ */
+void wlmtk_titlebar_button_destroy(
+    wlmtk_titlebar_button_t *titlebar_button_ptr)
+{
+    wlr_buffer_drop_nullify(
+        &titlebar_button_ptr->focussed_released_wlr_buffer_ptr);
+    wlr_buffer_drop_nullify(
+        &titlebar_button_ptr->focussed_pressed_wlr_buffer_ptr);
+    wlr_buffer_drop_nullify(
+        &titlebar_button_ptr->blurred_wlr_buffer_ptr);
+
+    wlmtk_button_fini(&titlebar_button_ptr->super_button);
+    free(titlebar_button_ptr);
+}
+
+/* ------------------------------------------------------------------------- */
+/**
+ * Redraws the titlebar button for given textures, position and style.
+ *
+ * @param titlebar_button_ptr
+ * @param focussed_gfxbuf_ptr
+ * @param blurred_gfxbuf_ptr
+ * @param position
+ * @param style_ptr
+ *
+ * @return true on success.
+ */
+bool wlmtk_titlebar_button_redraw(
+    wlmtk_titlebar_button_t *titlebar_button_ptr,
+    bs_gfxbuf_t *focussed_gfxbuf_ptr,
+    bs_gfxbuf_t *blurred_gfxbuf_ptr,
+    int position,
+    const wlmtk_titlebar_style_t *style_ptr)
+{
+    BS_ASSERT(focussed_gfxbuf_ptr->width == blurred_gfxbuf_ptr->width);
+    BS_ASSERT(focussed_gfxbuf_ptr->height == blurred_gfxbuf_ptr->height);
+    BS_ASSERT(style_ptr->height == focussed_gfxbuf_ptr->height);
+    BS_ASSERT(position + style_ptr->height <= focussed_gfxbuf_ptr->width);
+
+    struct wlr_buffer *focussed_released_ptr = create_buf(
+        focussed_gfxbuf_ptr, 0, false, style_ptr,
+        titlebar_button_ptr->draw);
+    struct wlr_buffer *focussed_pressed_ptr = create_buf(
+        focussed_gfxbuf_ptr, 0, true, style_ptr,
+        titlebar_button_ptr->draw);
+    struct wlr_buffer *blurred_ptr = create_buf(
+        blurred_gfxbuf_ptr, 0, false, style_ptr,
+        titlebar_button_ptr->draw);
+
+    if (NULL != focussed_released_ptr &&
+        NULL != focussed_pressed_ptr &&
+        NULL != blurred_ptr) {
+        wlr_buffer_drop_nullify(
+            &titlebar_button_ptr->focussed_released_wlr_buffer_ptr);
+        wlr_buffer_drop_nullify(
+            &titlebar_button_ptr->focussed_pressed_wlr_buffer_ptr);
+        wlr_buffer_drop_nullify(
+            &titlebar_button_ptr->blurred_wlr_buffer_ptr);
+
+        titlebar_button_ptr->focussed_released_wlr_buffer_ptr =
+            focussed_released_ptr;
+        titlebar_button_ptr->focussed_pressed_wlr_buffer_ptr =
+            focussed_pressed_ptr;
+        titlebar_button_ptr->blurred_wlr_buffer_ptr = blurred_ptr;
+
+        // FIXME: Depend on focus/blur.
+        wlmtk_button_set(
+            &titlebar_button_ptr->super_button,
+            titlebar_button_ptr->focussed_released_wlr_buffer_ptr,
+            titlebar_button_ptr->focussed_pressed_wlr_buffer_ptr);
+
+        return true;
+    }
+
+    wlr_buffer_drop_nullify(&focussed_released_ptr);
+    wlr_buffer_drop_nullify(&focussed_pressed_ptr);
+    wlr_buffer_drop_nullify(&blurred_ptr);
+    return false;
+}
+
+/* ------------------------------------------------------------------------- */
+/**
+ * Returns the titlebar button's super element.
+ *
+ * @param titlebar_button_ptr
+ *
+ * @return Pointer to the superclass @ref wlmtk_element_t.
+ */
+wlmtk_element_t *wlmtk_titlebar_button_element(
+    wlmtk_titlebar_button_t *titlebar_button_ptr)
+{
+    return &titlebar_button_ptr->super_button.super_buffer.super_element;
+}
+
+/* ------------------------------------------------------------------------- */
+/** Virtual destructor, wraps to @ref wlmtk_titlebar_button_destroy. */
+void titlebar_button_destroy(wlmtk_button_t *button_ptr)
+{
+    wlmtk_titlebar_button_t *titlebar_button_ptr = BS_CONTAINER_OF(
+        button_ptr, wlmtk_titlebar_button_t, super_button);
+    wlmtk_titlebar_button_destroy(titlebar_button_ptr);
+}
+
+/* ------------------------------------------------------------------------- */
+/** Helper: Creates a WLR buffer for the button. */
+struct wlr_buffer *create_buf(
+    bs_gfxbuf_t *gfxbuf_ptr,
+    int position,
+    bool pressed,
+    const wlmtk_titlebar_style_t *style_ptr,
+    void (*draw)(cairo_t *cairo_ptr, uint32_t color))
+{
+    struct wlr_buffer *wlr_buffer_ptr = bs_gfxbuf_create_wlr_buffer(
+        style_ptr->height, style_ptr->height);
+    if (NULL == wlr_buffer_ptr) return NULL;
+
+    bs_gfxbuf_copy_area(
+        bs_gfxbuf_from_wlr_buffer(wlr_buffer_ptr), 0, 0,
+        gfxbuf_ptr, position, 0, style_ptr->height, style_ptr->height);
+
+    cairo_t *cairo_ptr = cairo_create_from_wlr_buffer(wlr_buffer_ptr);
+    if (NULL == cairo_ptr) {
+        wlr_buffer_drop(wlr_buffer_ptr);
+        return NULL;
+    }
+    wlmaker_primitives_draw_bezel(
+        cairo_ptr, style_ptr->bezel_width, !pressed);
+    draw(cairo_ptr, style_ptr->focussed_text_color);
+    cairo_destroy(cairo_ptr);
+
+    return wlr_buffer_ptr;
+}
 
 /* == Unit tests =========================================================== */
 
