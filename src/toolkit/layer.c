85ed13676b18d0c74e7617f3abde3eaccbac6ec7
Philipp Kaeser <kaeser@gubbe.ch>
2024-04-25 21:18:02 +0200

    Adds boilerplate for wlmtk_layer_t.

---
+++ src/toolkit/layer.c
@@ -0,0 +1,64 @@
+/* ========================================================================= */
+/**
+ * @file layer.c
+ *
+ * @copyright
+ * Copyright 2023 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include "layer.h"
+
+/* == Declarations ========================================================= */
+
+/** State of a layer. */
+struct _wlmtk_layer_t {
+    /** Super class of the box. */
+    wlmtk_container_t         super_container;
+    /** Virtual method table of the superclass' container. */
+    wlmtk_container_vmt_t     orig_super_container_vmt;
+};
+
+/* == Exported methods ===================================================== */
+
+/* ------------------------------------------------------------------------- */
+wlmtk_layer_t *wlmtk_layer_create(wlmtk_env_t *env_ptr)
+{
+    wlmtk_layer_t *layer_ptr = logged_calloc(1, sizeof(wlmtk_layer_t));
+    if (NULL == layer_ptr) return NULL;
+
+    if (!wlmtk_container_init(&layer_ptr->super_container, env_ptr)) {
+        wlmtk_layer_destroy(layer_ptr);
+        return NULL;
+    }
+
+    return layer_ptr;
+}
+
+/* ------------------------------------------------------------------------- */
+void wlmtk_layer_destroy(wlmtk_layer_t *layer_ptr)
+{
+    wlmtk_container_fini(&layer_ptr->super_container);
+    free(layer_ptr);
+}
+
+/* ------------------------------------------------------------------------- */
+wlmtk_container_t *wlmtk_layer_container(wlmtk_layer_t *layer_ptr)
+{
+    return &layer_ptr->super_container;
+}
+
+/* == Local (static) methods =============================================== */
+
+/* == End of layer.c ======================================================= */
---
+++ src/toolkit/layer.h
@@ -0,0 +1,57 @@
+/* ========================================================================= */
+/**
+ * @file layer.h
+ *
+ * @copyright
+ * Copyright 2023 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+#ifndef __LAYER_H__
+#define __LAYER_H__
+
+/** Forward declaration: Layer state. */
+typedef struct _wlmtk_layer_t wlmtk_layer_t;
+
+#include "env.h"
+#include "container.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif  // __cplusplus
+
+/**
+ * Creates a layer. Layers contain panels, such as layer shells.
+ *
+ * @param env_ptr
+ *
+ * @return Pointer to the layer handle or NULL on error.
+ */
+wlmtk_layer_t *wlmtk_layer_create(wlmtk_env_t *env_ptr);
+
+/**
+ * Destroys the layer.
+ *
+ * @param layer_ptr
+ */
+void wlmtk_layer_destroy(wlmtk_layer_t *layer_ptr);
+
+/** @return Pointer to @ref wlmtk_layer_t::super_container. */
+wlmtk_container_t *wlmtk_layer_container(wlmtk_layer_t *layer_ptr);
+
+#ifdef __cplusplus
+}  // extern "C"
+#endif  // __cplusplus
+
+#endif /* __LAYER_H__ */
+/* == End of layer.h ======================================================= */
