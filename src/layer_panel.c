360952eb5dc020367e065702fec69d5f37391d15
Philipp Kaeser <kaeser@gubbe.ch>
2024-05-01 15:51:30 +0200

    Adds a commit handler for the layer surface, to handle updates.

--- src/layer_panel.c
+++ src/layer_panel.c
@@ -41,6 +41,9 @@ struct _wlmaker_layer_panel_t {
     /** Listener for the `unmap` signal raised by `wlmtk_surface_t`. */
     struct wl_listener        surface_unmap_listener;
 
+    /** Listener for the `commit` signal raised by `wlr_surface`. */
+    struct wl_listener        surface_commit_listener;
+
     /** Listener for the `destroy` signal raised by `wlr_layer_surface_v1`. */
     struct wl_listener        destroy_listener;
     /** Listener for `new_popup` signal raised by `wlr_layer_surface_v1`. */
@@ -54,7 +57,8 @@ static wlmaker_layer_panel_t *_wlmaker_layer_panel_create_injected(
 static void _wlmaker_layer_panel_destroy(
     wlmaker_layer_panel_t *layer_panel_ptr);
 
-static wlmtk_workspace_layer_t _wlmaker_layer_from_zwlr_layer(
+static bool _wlmaker_layer_panel_apply_layer(
+    wlmaker_layer_panel_t *layer_panel_ptr,
     enum zwlr_layer_shell_v1_layer zwlr_layer);
 static void _wlmaker_layer_panel_set_positioning(
     wlmtk_panel_positioning_t *positioning_ptr,
@@ -65,6 +69,9 @@ static uint32_t _wlmaker_layer_panel_request_size(
     int width,
     int height);
 
+static void _wlmaker_layer_panel_handle_surface_commit(
+    struct wl_listener *listener_ptr,
+    void *data_ptr);
 static void _wlmaker_layer_panel_handle_surface_map(
     struct wl_listener *listener_ptr,
     void *data_ptr);
@@ -146,6 +153,7 @@ wlmaker_layer_panel_t *_wlmaker_layer_panel_create_injected(
         wlmtk_surface_element(layer_panel_ptr->wlmtk_surface_ptr));
     wlmtk_element_set_visible(
         wlmtk_surface_element(layer_panel_ptr->wlmtk_surface_ptr), true);
+
     wlmtk_surface_connect_map_listener_signal(
         layer_panel_ptr->wlmtk_surface_ptr,
         &layer_panel_ptr->surface_map_listener,
@@ -154,6 +162,11 @@ wlmaker_layer_panel_t *_wlmaker_layer_panel_create_injected(
         layer_panel_ptr->wlmtk_surface_ptr,
         &layer_panel_ptr->surface_unmap_listener,
         _wlmaker_layer_panel_handle_surface_unmap);
+
+    wlmtk_util_connect_listener_signal(
+        &wlr_layer_surface_v1_ptr->surface->events.commit,
+        &layer_panel_ptr->surface_commit_listener,
+        _wlmaker_layer_panel_handle_surface_commit);
 
     wlmtk_util_connect_listener_signal(
         &wlr_layer_surface_v1_ptr->events.destroy,
@@ -175,25 +188,12 @@ wlmaker_layer_panel_t *_wlmaker_layer_panel_create_injected(
         return NULL;
     }
 
-    wlmaker_workspace_t *workspace_ptr = wlmaker_server_get_current_workspace(
-        layer_panel_ptr->server_ptr);
-    wlmtk_workspace_t *wlmtk_workspace_ptr = wlmaker_workspace_wlmtk(
-        workspace_ptr);
-    wlmtk_workspace_layer_t layer = _wlmaker_layer_from_zwlr_layer(
-        layer_panel_ptr->wlr_layer_surface_v1_ptr->current.layer);
-    wlmtk_layer_t *layer_ptr = wlmtk_workspace_get_layer(
-        wlmtk_workspace_ptr, layer);
-    if (NULL == layer_ptr) {
-        wl_resource_post_error(
-            layer_panel_ptr->wlr_layer_surface_v1_ptr->resource,
-            WL_DISPLAY_ERROR_INVALID_METHOD,
-            "Invalid zwlr_layer value: %d",
-            layer_panel_ptr->wlr_layer_surface_v1_ptr->current.layer);
+    if (!_wlmaker_layer_panel_apply_layer(
+            layer_panel_ptr,
+            layer_panel_ptr->wlr_layer_surface_v1_ptr->pending.layer)) {
         _wlmaker_layer_panel_destroy(layer_panel_ptr);
         return NULL;
     }
-
-    wlmtk_layer_add_panel(layer_ptr, &layer_panel_ptr->super_panel);
 
     bs_log(BS_INFO, "Created layer panel %p with wlmtk surface %p",
            layer_panel_ptr, layer_panel_ptr->wlmtk_surface_ptr);
@@ -217,7 +217,10 @@ void _wlmaker_layer_panel_destroy(wlmaker_layer_panel_t *layer_panel_ptr)
         wlmtk_layer_remove_panel(layer_ptr, &layer_panel_ptr->super_panel);
     }
 
+    wlmtk_util_disconnect_listener(&layer_panel_ptr->new_popup_listener);
     wlmtk_util_disconnect_listener(&layer_panel_ptr->destroy_listener);
+
+    wlmtk_util_disconnect_listener(&layer_panel_ptr->surface_commit_listener);
 
     wlmtk_util_disconnect_listener(&layer_panel_ptr->surface_unmap_listener);
     wlmtk_util_disconnect_listener(&layer_panel_ptr->surface_map_listener);
@@ -261,6 +264,60 @@ wlmtk_workspace_layer_t _wlmaker_layer_from_zwlr_layer(
 }
 
 /* ------------------------------------------------------------------------- */
+/** Updates the layer this panel is part of. Posts an error if invalid. */
+bool _wlmaker_layer_panel_apply_layer(
+    wlmaker_layer_panel_t *layer_panel_ptr,
+    enum zwlr_layer_shell_v1_layer zwlr_layer)
+{
+    wlmtk_workspace_layer_t layer;
+    switch (zwlr_layer) {
+    case ZWLR_LAYER_SHELL_V1_LAYER_BACKGROUND:
+        layer = WLMTK_WORKSPACE_LAYER_BACKGROUND;
+        break;
+    case ZWLR_LAYER_SHELL_V1_LAYER_BOTTOM:
+        layer = WLMTK_WORKSPACE_LAYER_BOTTOM;
+        break;
+    case ZWLR_LAYER_SHELL_V1_LAYER_TOP:
+        layer = WLMTK_WORKSPACE_LAYER_TOP;
+        break;
+    case ZWLR_LAYER_SHELL_V1_LAYER_OVERLAY:
+        layer = WLMTK_WORKSPACE_LAYER_OVERLAY;
+        break;
+    default:
+        wl_resource_post_error(
+            layer_panel_ptr->wlr_layer_surface_v1_ptr->resource,
+            WL_DISPLAY_ERROR_INVALID_METHOD,
+            "Invalid value for for zwlr_layer value: %d",
+            layer_panel_ptr->wlr_layer_surface_v1_ptr->pending.layer);
+        return false;
+    }
+
+    wlmaker_workspace_t *workspace_ptr = wlmaker_server_get_current_workspace(
+        layer_panel_ptr->server_ptr);
+    wlmtk_workspace_t *wlmtk_workspace_ptr = wlmaker_workspace_wlmtk(
+        workspace_ptr);
+    wlmtk_layer_t *layer_ptr = wlmtk_workspace_get_layer(
+        wlmtk_workspace_ptr, layer);
+
+    wlmtk_layer_t *current_layer_ptr = wlmtk_panel_get_layer(
+        &layer_panel_ptr->super_panel);
+    if (layer_ptr == current_layer_ptr) return true;
+
+    if (NULL != current_layer_ptr) {
+        wlmtk_layer_remove_panel(
+            current_layer_ptr, &layer_panel_ptr->super_panel);
+    }
+
+    if (NULL != layer_ptr) {
+        wlmtk_layer_add_panel(
+            layer_ptr, &layer_panel_ptr->super_panel);
+    }
+
+    return true;
+}
+
+
+/* ------------------------------------------------------------------------- */
 /** Updates `positioning_ptr` from the given surface state. */
 void _wlmaker_layer_panel_set_positioning(
     wlmtk_panel_positioning_t *positioning_ptr,
@@ -293,7 +350,32 @@ uint32_t _wlmaker_layer_panel_request_size(
         layer_panel_ptr->wlr_layer_surface_v1_ptr, width, height);
 }
 
-// FIXME: Add 'commit' handler, and update state when required.
+/* ------------------------------------------------------------------------- */
+/**
+ * Handler for the `commit` signal of `wlr_surface`.
+ *
+ * Updates positioning and layer of the panel, as required.
+ *
+ * @param listener_ptr
+ * @param data_ptr
+ */
+void _wlmaker_layer_panel_handle_surface_commit(
+    struct wl_listener *listener_ptr,
+    __UNUSED__ void *data_ptr)
+{
+    wlmaker_layer_panel_t *layer_panel_ptr = BS_CONTAINER_OF(
+        listener_ptr, wlmaker_layer_panel_t, surface_commit_listener);
+
+    wlmtk_panel_positioning_t pos;
+    _wlmaker_layer_panel_set_positioning(
+        &pos, &layer_panel_ptr->wlr_layer_surface_v1_ptr->pending);
+    // FIXME: call into panel.
+
+    // Update layer, if needed. Ignore return values here.
+    _wlmaker_layer_panel_apply_layer(
+        layer_panel_ptr,
+        layer_panel_ptr->wlr_layer_surface_v1_ptr->pending.layer);
+}
 
 /* ------------------------------------------------------------------------- */
 /**
