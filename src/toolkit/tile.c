6a69e9ff2abc597ee349ffe16a138fa74aae7b80
Philipp Kaeser <kaeser@gubbe.ch>
2024-06-02 12:56:58 +0200

    Adds boilerplate implementation for tile.

--- src/toolkit/tile.c
+++ src/toolkit/tile.c
@@ -22,11 +22,52 @@
 
 /* == Declarations ========================================================= */
 
+static void _wlmtk_tile_update_layout(wlmtk_container_t *container_ptr);
+
 /* == Data ================================================================= */
+
+/** Virtual methods implemented by @ref wlmtk_tile_t. */
+static const wlmtk_container_vmt_t _wlmtk_tile_container_vmt = {
+    .update_layout = _wlmtk_tile_update_layout
+};
 
 /* == Exported methods ===================================================== */
 
+/* ------------------------------------------------------------------------- */
+bool wlmtk_tile_init(
+    wlmtk_tile_t *tile_ptr,
+    wlmtk_env_t *env_ptr)
+{
+    memset(tile_ptr, 0, sizeof(wlmtk_tile_t));
+    if (!wlmtk_container_init(&tile_ptr->super_container, env_ptr)) {
+        wlmtk_tile_fini(tile_ptr);
+        return false;
+    }
+    tile_ptr->orig_super_container_vmt = wlmtk_container_extend(
+        &tile_ptr->super_container, &_wlmtk_tile_container_vmt);
+
+    return true;
+}
+
+/* ------------------------------------------------------------------------- */
+void wlmtk_tile_fini(wlmtk_tile_t *tile_ptr)
+{
+    wlmtk_container_fini(&tile_ptr->super_container);
+}
+
+/* ------------------------------------------------------------------------- */
+wlmtk_element_t *wlmtk_tile_elmeent(wlmtk_tile_t *tile_ptr)
+{
+    return &tile_ptr->super_container.super_element;
+}
+
 /* == Local (static) methods =============================================== */
+
+/* ------------------------------------------------------------------------- */
+/** Handles requests to update layout. Called when elements are added. */
+void _wlmtk_tile_update_layout(__UNUSED__ wlmtk_container_t *container_ptr)
+{
+}
 
 /* == Unit tests =========================================================== */
 
