6bcc01e4991a6b5cc8019c6270593296a3956a5e
Philipp Kaeser <kaeser@gubbe.ch>
2024-06-09 17:12:12 +0200

    Adds the subprocess-start sequence from the earlier dock app to wlmaker_launcher_t.

--- src/launcher.c
+++ src/launcher.c
@@ -25,10 +25,16 @@ struct _wlmaker_launcher_t {
     /** Image element. One element of @ref wlmaker_launcher_t::super_tile. */
     wlmtk_image_t             *image_ptr;
 
+    /** Back-link to the server. */
+    wlmaker_server_t          *server_ptr;
+
     /** Commandline to launch the associated application. */
     char                      *cmdline_ptr;
     /** Path to the icon. */
     char                      *icon_path_ptr;
+
+    /** Subprocesses that were created by this launcher. */
+    bs_ptr_set_t              *subprocesses_ptr;
 };
 
 /** Plist descroptor for a launcher. */
@@ -57,6 +63,8 @@ static bool _wlmaker_launcher_pointer_button(
     wlmtk_element_t *element_ptr,
     const wlmtk_button_event_t *button_event_ptr);
 
+static void _wlmaker_launcher_start(wlmaker_launcher_t *launcher_ptr);
+
 /* == Data ================================================================= */
 
 /** The launcher's extension to @ref wlmtk_element_t virtual method table. */
@@ -75,6 +83,7 @@ wlmaker_launcher_t *wlmaker_launcher_create_from_plist(
     wlmaker_launcher_t *launcher_ptr = logged_calloc(
         1, sizeof(wlmaker_launcher_t));
     if (NULL == launcher_ptr) return NULL;
+    launcher_ptr->server_ptr = server_ptr;
 
     if (!wlmtk_tile_init(&launcher_ptr->super_tile,
                          style_ptr,
@@ -91,6 +100,12 @@ wlmaker_launcher_t *wlmaker_launcher_create_from_plist(
     if (!wlmcfg_decode_dict(
             dict_ptr, _wlmaker_launcher_plist_desc, launcher_ptr)) {
         bs_log(BS_ERROR, "Failed to create launcher from plist dict.");
+        wlmaker_launcher_destroy(launcher_ptr);
+        return NULL;
+    }
+
+    launcher_ptr->subprocesses_ptr = bs_ptr_set_create();
+    if (NULL == launcher_ptr->subprocesses_ptr) {
         wlmaker_launcher_destroy(launcher_ptr);
         return NULL;
     }
@@ -129,6 +144,20 @@ void wlmaker_launcher_destroy(wlmaker_launcher_t *launcher_ptr)
         wlmtk_tile_set_content(&launcher_ptr->super_tile, NULL);
         wlmtk_image_destroy(launcher_ptr->image_ptr);
         launcher_ptr->image_ptr = NULL;
+    }
+
+    if (NULL != launcher_ptr->subprocesses_ptr) {
+        wlmaker_subprocess_handle_t *subprocess_handle_ptr;
+        while (NULL != (subprocess_handle_ptr = bs_ptr_set_any(
+                            launcher_ptr->subprocesses_ptr))) {
+            wlmaker_subprocess_monitor_cede(
+                launcher_ptr->server_ptr->monitor_ptr,
+                subprocess_handle_ptr);
+            bs_ptr_set_erase(launcher_ptr->subprocesses_ptr,
+                             subprocess_handle_ptr);
+        }
+        bs_ptr_set_destroy(launcher_ptr->subprocesses_ptr);
+        launcher_ptr->subprocesses_ptr = NULL;
     }
 
     if (NULL != launcher_ptr->cmdline_ptr) {
@@ -173,9 +202,56 @@ bool _wlmaker_launcher_pointer_button(
     if (BTN_LEFT != button_event_ptr->button) return true;
     if (WLMTK_BUTTON_CLICK != button_event_ptr->type) return true;
 
-    bs_log(BS_INFO, "FIXME: Click launcher %p!", launcher_ptr);
-
+    _wlmaker_launcher_start(launcher_ptr);
     return true;
+}
+
+/* ------------------------------------------------------------------------- */
+/**
+ * Starts the application, called when the launcher is clicked.
+ *
+ * @param launcher_ptr
+ */
+void _wlmaker_launcher_start(wlmaker_launcher_t *launcher_ptr)
+{
+    bs_subprocess_t *subprocess_ptr = bs_subprocess_create_cmdline(
+        launcher_ptr->cmdline_ptr);
+    if (NULL == subprocess_ptr) {
+        bs_log(BS_ERROR, "Failed bs_subprocess_create_cmdline(%s)",
+               launcher_ptr->cmdline_ptr);
+        return;
+    }
+
+    if (!bs_subprocess_start(subprocess_ptr)) {
+        bs_log(BS_ERROR, "Failed bs_subprocess_start for %s",
+               launcher_ptr->cmdline_ptr);
+        bs_subprocess_destroy(subprocess_ptr);
+        return;
+    }
+
+#if 0
+    wlmaker_subprocess_handle_t *subprocess_handle_ptr;
+    subprocess_handle_ptr = wlmaker_subprocess_monitor_entrust(
+        dock_app_ptr->view_ptr->server_ptr->monitor_ptr,
+        subprocess_ptr,
+        handle_terminated,
+        dock_app_ptr,
+        handle_window_created,
+        handle_window_mapped,
+        handle_window_unmapped,
+        handle_window_destroyed);
+
+    if (!bs_ptr_set_insert(launcher_ptr->subprocesses_ptr,
+                           subprocess_handle_ptr)) {
+        bs_log(BS_WARNING, "Launcher %p: Failed bs_ptr_set_insert(%p, %p). "
+               "Will not show status of subprocess in App.",
+               launcher_ptr, launcher_ptr->subprocesses_ptr,
+               subprocess_handle_ptr);
+        wlmaker_subprocess_monitor_cede(
+            launcher_ptr->server_ptr->monitor_ptr,
+            subprocess_handle_ptr);
+    }
+#endif
 }
 
 /* == Unit tests =========================================================== */
