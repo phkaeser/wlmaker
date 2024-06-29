e76af9c3920a72979f2ff4b544db6c7b39a9b712
Philipp Kaeser <kaeser@gubbe.ch>
2024-06-29 17:51:19 +0300

    Removes wlmaker_server_get_current_workspace in wlmaker_keyboard_t.

--- src/keyboard.c
+++ src/keyboard.c
@@ -275,9 +275,8 @@ void handle_key(struct wl_listener *listener_ptr, void *data_ptr)
     if (processed) return;
 
     processed = wlmtk_element_keyboard_event(
-        wlmtk_workspace_element(
-            wlmaker_workspace_wlmtk(wlmaker_server_get_current_workspace(
-                                        keyboard_ptr->server_ptr))),
+        wlmtk_workspace_element(wlmaker_server_get_current_wlmkt_workspace(
+                                    keyboard_ptr->server_ptr)),
         wlr_keyboard_key_event_ptr,
         NULL,
         0,
