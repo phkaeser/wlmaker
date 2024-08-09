f83ff5cb129838802ffaad556e506d98c1046eca
Philipp Kaeser <kaeser@gubbe.ch>
2024-08-09 22:01:44 +0200

    Adapts to new wlroots-0.18 API. XDG toplevels seem fine, but popups are broken.

--- src/xdg_shell.c
+++ src/xdg_shell.c
@@ -38,9 +38,6 @@ static void handle_new_toplevel(
 static void handle_new_popup(
     struct wl_listener *listener_ptr,
     void *data_ptr);
-static void handle_new_surface(
-    struct wl_listener *listener_ptr,
-    void *data_ptr);
 
 /* == Exported methods ===================================================== */
 
@@ -59,10 +56,6 @@ wlmaker_xdg_shell_t *wlmaker_xdg_shell_create(wlmaker_server_t *server_ptr)
         return NULL;
     }
 
-    wlmtk_util_connect_listener_signal(
-        &xdg_shell_ptr->wlr_xdg_shell_ptr->events.new_surface,
-        &xdg_shell_ptr->new_surface_listener,
-        handle_new_surface);
     wlmtk_util_connect_listener_signal(
         &xdg_shell_ptr->wlr_xdg_shell_ptr->events.new_toplevel,
         &xdg_shell_ptr->new_toplevel_listener,
@@ -85,7 +78,6 @@ void wlmaker_xdg_shell_destroy(wlmaker_xdg_shell_t *xdg_shell_ptr)
     wl_list_remove(&xdg_shell_ptr->destroy_listener.link);
     wl_list_remove(&xdg_shell_ptr->new_popup_listener.link);
     wl_list_remove(&xdg_shell_ptr->new_toplevel_listener.link);
-    wl_list_remove(&xdg_shell_ptr->new_surface_listener.link);
     free(xdg_shell_ptr);
 }
 
@@ -112,18 +104,21 @@ void handle_destroy(struct wl_listener *listener_ptr,
  * Event handler for the `new_toplevel` signal raised by `wlr_xdg_shell`.
  *
  * @param listener_ptr
- * @param data_ptr
+ * @param data_ptr            Points to the new struct wlr_xdg_toplevel.
  */
 void handle_new_toplevel(struct wl_listener *listener_ptr,
-                    __UNUSED__ void *data_ptr)
+                         void *data_ptr)
 {
     wlmaker_xdg_shell_t *xdg_shell_ptr = BS_CONTAINER_OF(
         listener_ptr, wlmaker_xdg_shell_t, new_toplevel_listener);
     struct wlr_xdg_toplevel *wlr_xdg_toplevel_ptr = data_ptr;
 
-    bs_log(BS_ERROR, "FIXME: New toplevel %p for shell %p",
-           wlr_xdg_toplevel_ptr, xdg_shell_ptr);
+    wlmtk_window_t *window_ptr = wlmtk_window_create_from_xdg_toplevel(
+        wlr_xdg_toplevel_ptr, xdg_shell_ptr->server_ptr);
 
+    // TODO(kaeser@gubbe.ch): Handle errors.
+    bs_log(BS_INFO, "XDG shell: Toolkit window %p for toplevel %p",
+           window_ptr, wlr_xdg_toplevel_ptr);
 }
 
 /* ------------------------------------------------------------------------- */
@@ -131,57 +126,19 @@ void handle_new_toplevel(struct wl_listener *listener_ptr,
  * Event handler for the `new_popup` signal raised by `wlr_xdg_shell`.
  *
  * @param listener_ptr
- * @param data_ptr
+ * @param data_ptr            Points to the new struct wlr_xdg_popup.
  */
 void handle_new_popup(struct wl_listener *listener_ptr,
-                    __UNUSED__ void *data_ptr)
+                      void *data_ptr)
 {
     wlmaker_xdg_shell_t *xdg_shell_ptr = BS_CONTAINER_OF(
         listener_ptr, wlmaker_xdg_shell_t, new_popup_listener);
     struct wlr_xdg_popup *wlr_xdg_popup_ptr = data_ptr;
 
-    __UNUSED__ wlmaker_xdg_popup_t *wlmaker_xdg_popup_ptr = wlmaker_xdg_popup_create(
-        wlr_xdg_popup_ptr, xdg_shell_ptr->server_ptr->env_ptr);
-
-    // FIXME: Find parent from wlr_xdg_popop_ptr->parent, and attach there.
-    bs_log(BS_ERROR, "FIXME: New popup %p for shell %p",
-           wlr_xdg_popup_ptr, xdg_shell_ptr);
-}
-
-/* ------------------------------------------------------------------------- */
-/**
- * Event handler for the `new_surface` signal raised by `wlr_xdg_shell`.
- *
- * @param listener_ptr
- * @param data_ptr
- */
-void handle_new_surface(struct wl_listener *listener_ptr,
-                        void *data_ptr)
-{
-    struct wlr_xdg_surface *wlr_xdg_surface_ptr;
-
-    wlmaker_xdg_shell_t *xdg_shell_ptr = BS_CONTAINER_OF(
-        listener_ptr, wlmaker_xdg_shell_t, new_surface_listener);
-    wlr_xdg_surface_ptr = data_ptr;
-
-    switch (wlr_xdg_surface_ptr->role) {
-    case WLR_XDG_SURFACE_ROLE_POPUP:
-        // We're dealing with popups separately -- via the `new_popup` signal
-        // from the `wlr_xdg_surface` for popups as children of XDG shell
-        // surfaces, respectively the `new_popup` signal of the
-        // `wlr_scene_layer_surface` for popups of the WLR layer surface.
-        break;
-
-    case WLR_XDG_SURFACE_ROLE_TOPLEVEL:;
-
-        wlmtk_window_t *window_ptr = wlmtk_window_create_from_xdg_toplevel(
-            wlr_xdg_surface_ptr, xdg_shell_ptr->server_ptr);
-        bs_log(BS_INFO, "XDG shell: Toolkit window %p for surface %p",
-               window_ptr, wlr_xdg_surface_ptr);
-        break;
-
-    default:
-        bs_log(BS_ERROR, "Unhandled role: %d", wlr_xdg_surface_ptr->role);
+    if (NULL == wlr_xdg_popup_ptr->parent) {
+        bs_log(BS_WARNING,
+               "Unimplemented: XDG shell %p: Creating popup %p without parent",
+               xdg_shell_ptr, wlr_xdg_popup_ptr);
     }
 }
 
