8d88c48d7e4d548cac00256e318692c90459384f
Philipp Kaeser <kaeser@gubbe.ch>
2024-08-27 15:54:11 -0400

    Places window title relative to size, slightly improving the optics.

--- src/toolkit/primitives.c
+++ src/toolkit/primitives.c
@@ -247,7 +247,10 @@ void wlmaker_primitives_draw_window_title(
     cairo_set_font_size(cairo_ptr, font_style_ptr->size);
     cairo_set_source_argb8888(cairo_ptr, color);
 
-    cairo_move_to(cairo_ptr, 6, 2 + font_style_ptr->size);
+    cairo_move_to(
+        cairo_ptr,
+        font_style_ptr->size * 6 / 15,
+        font_style_ptr->size * 2 / 15 + font_style_ptr->size);
     cairo_show_text(cairo_ptr, title_ptr ? title_ptr : "Unnamed");
     cairo_restore(cairo_ptr);
 }
