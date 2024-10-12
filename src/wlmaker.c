3b8ac8819dd3e843a597890e6f66fc00ab99a80f
Philipp Kaeser <130065133+phkaeser@users.noreply.github.com>
2024-10-12 10:37:05 -0400

    Fixes double free of config dicts. (#126)
    * Fixes unref without having had the ref.
    
    * Records the bugfix in the roadmap.

--- src/wlmaker.c
+++ src/wlmaker.c
@@ -408,7 +408,7 @@ int main(__UNUSED__ int argc, __UNUSED__ const char **argv)
     }
     bs_ptr_stack_fini(&wlmaker_subprocess_stack);
 
-    // FIXME -- double-free..? wlmcfg_dict_unref(config_dict_ptr);
+    wlmcfg_dict_unref(config_dict_ptr);
     wlmcfg_dict_unref(state_dict_ptr);
     regfree(&wlmaker_wlr_log_regex);
     return rv;
