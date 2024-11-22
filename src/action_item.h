41e43396cb22841cccfb281d10ba06fd68d34631
Philipp Kaeser <130065133+phkaeser@users.noreply.github.com>
2024-11-22 11:59:10 +0200

    Fixes the copyright statement in a few files: This should have been with Google LLC from the start. (#139)

--- src/action_item.h
+++ src/action_item.h
@@ -1,7 +1,21 @@
 /* ========================================================================= */
 /**
  * @file action_item.h
- * Copyright (c) 2024 by Philipp Kaeser <kaeser@gubbe.ch>
+ *
+ * @copyright
+ * Copyright 2024 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 #ifndef __WLMAKER_ACTION_ITEM_H__
 #define __WLMAKER_ACTION_ITEM_H__
