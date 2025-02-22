142d46864a90f6e3abc766f85347558f3d015df9
Philipp Kaeser <kaeser@gubbe.ch>
2025-02-22 17:55:38 +0100

    Updates wlmtk_submenu_t to using it's item in composed fashion.

--- src/toolkit/submenu.c
+++ src/toolkit/submenu.c
@@ -21,31 +21,33 @@
 #include "submenu.h"
 
 #include "popup_menu.h"
+#include "util.h"
 
 /* == Declarations ========================================================= */
 
 struct _wlmtk_submenu_t {
-    wlmtk_menu_item_t         super_menu_item;
+    wlmtk_menu_item_t         *menu_item_ptr;
 
     wlmtk_popup_menu_t        *popup_menu_ptr;
 
 
-    wlmtk_menu_item_t         item1;
-    wlmtk_menu_item_t         item2;
+    wlmtk_menu_item_t         *item1_ptr;
+    wlmtk_menu_item_t         *item2_ptr;
+
+    /** Listener for @ref wlmtk_menu_item_events_t::triggered. */
+    struct wl_listener        triggered_listener;
+    /** Listener for @ref wlmtk_menu_item_events_t::destroy. */
+    struct wl_listener        destroy_listener;
 };
 
-static void _wlmtk_submenu_element_destroy(wlmtk_element_t *element_ptr);
-static void _wlmtk_submenu_menu_item_clicked(wlmtk_menu_item_t *menu_item_ptr);
+static void _wlmtk_submenu_handle_triggered(
+    struct wl_listener *listener_ptr,
+    void *data_ptr);
+static void _wlmtk_submenu_handle_destroy(
+    struct wl_listener *listener_ptr,
+    void *data_ptr);
 
 /* == Data ================================================================= */
-
-static const wlmtk_element_vmt_t _wlmtk_submenu_element_vmt = {
-    .destroy = _wlmtk_submenu_element_destroy
-};
-
-static const wlmtk_menu_item_vmt_t _wlmtk_submenu_menu_item_vmt = {
-    .clicked = _wlmtk_submenu_menu_item_clicked
-};
 
 /* == Exported methods ===================================================== */
 
@@ -58,21 +60,20 @@ wlmtk_submenu_t *wlmtk_submenu_create(
     wlmtk_submenu_t *submenu_ptr = logged_calloc(1, sizeof(wlmtk_submenu_t));
     if (NULL == submenu_ptr) return NULL;
 
-    if (!wlmtk_menu_item_init(
-            &submenu_ptr->super_menu_item,
-            &style_ptr->item,
-            env_ptr)) {
+    submenu_ptr->menu_item_ptr = wlmtk_menu_item_create(
+        &style_ptr->item, env_ptr);
+    if (NULL == submenu_ptr->menu_item_ptr) {
         wlmtk_submenu_destroy(submenu_ptr);
         return NULL;
     }
-    wlmtk_element_extend(
-        wlmtk_menu_item_element(&submenu_ptr->super_menu_item),
-        &_wlmtk_submenu_element_vmt);
-    wlmtk_menu_item_extend(
-        &submenu_ptr->super_menu_item,
-        &_wlmtk_submenu_menu_item_vmt);
-    // TODO(kaeser@gubbe.ch): Should not be required!
-    submenu_ptr->super_menu_item.width = style_ptr->item.width;
+    wlmtk_util_connect_listener_signal(
+        &wlmtk_menu_item_events(submenu_ptr->menu_item_ptr)->triggered,
+        &submenu_ptr->triggered_listener,
+        _wlmtk_submenu_handle_triggered);
+    wlmtk_util_connect_listener_signal(
+        &wlmtk_menu_item_events(submenu_ptr->menu_item_ptr)->destroy,
+        &submenu_ptr->destroy_listener,
+        _wlmtk_submenu_handle_destroy);
 
     submenu_ptr->popup_menu_ptr = wlmtk_popup_menu_create(style_ptr, env_ptr);
     if (NULL == submenu_ptr->popup_menu_ptr) {
@@ -81,20 +82,18 @@ wlmtk_submenu_t *wlmtk_submenu_create(
     }
 
 
-    wlmtk_menu_item_set_text(&submenu_ptr->super_menu_item, "FIXME");
-    wlmtk_menu_item_init(&submenu_ptr->item1, &style_ptr->item, env_ptr);
-    wlmtk_menu_item_init(&submenu_ptr->item2, &style_ptr->item, env_ptr);
-    submenu_ptr->item1.width = style_ptr->item.width;
-    submenu_ptr->item2.width = style_ptr->item.width;
-    wlmtk_menu_item_set_text(&submenu_ptr->item1, "FIXME: Item 1");
-    wlmtk_menu_item_set_text(&submenu_ptr->item2, "FIXME: Item 2");
+    wlmtk_menu_item_set_text(submenu_ptr->menu_item_ptr, "FIXME");
+    submenu_ptr->item1_ptr = wlmtk_menu_item_create(&style_ptr->item, env_ptr);
+    submenu_ptr->item2_ptr = wlmtk_menu_item_create(&style_ptr->item, env_ptr);
+    wlmtk_menu_item_set_text(submenu_ptr->item1_ptr, "FIXME: Item 1");
+    wlmtk_menu_item_set_text(submenu_ptr->item2_ptr, "FIXME: Item 2");
 
     wlmtk_menu_add_item(
         wlmtk_popup_menu_menu(submenu_ptr->popup_menu_ptr),
-        &submenu_ptr->item1);
+        submenu_ptr->item1_ptr);
     wlmtk_menu_add_item(
         wlmtk_popup_menu_menu(submenu_ptr->popup_menu_ptr),
-        &submenu_ptr->item2);
+        submenu_ptr->item2_ptr);
 
     wlmtk_popup_add_popup(
         wlmtk_popup_menu_popup(parent_pum_ptr),
@@ -118,46 +117,59 @@ void wlmtk_submenu_destroy(wlmtk_submenu_t *submenu_ptr)
 
         wlmtk_menu_remove_item(
             wlmtk_popup_menu_menu(submenu_ptr->popup_menu_ptr),
-            &submenu_ptr->item1);
+            submenu_ptr->item1_ptr);
         wlmtk_menu_remove_item(
             wlmtk_popup_menu_menu(submenu_ptr->popup_menu_ptr),
-            &submenu_ptr->item2);
+            submenu_ptr->item2_ptr);
 
         wlmtk_popup_menu_destroy(submenu_ptr->popup_menu_ptr);
         submenu_ptr->popup_menu_ptr = NULL;
     }
 
-    wlmtk_menu_item_fini(&submenu_ptr->item2);
-    wlmtk_menu_item_fini(&submenu_ptr->item1);
+    wlmtk_menu_item_destroy(submenu_ptr->item2_ptr);
+    wlmtk_menu_item_destroy(submenu_ptr->item1_ptr);
 
-    wlmtk_menu_item_fini(&submenu_ptr->super_menu_item);
+    if (NULL == submenu_ptr->menu_item_ptr) {
+        wlmtk_util_disconnect_listener(&submenu_ptr->destroy_listener);
+        wlmtk_util_disconnect_listener(&submenu_ptr->triggered_listener);
+
+        wlmtk_menu_item_destroy(submenu_ptr->menu_item_ptr);
+        submenu_ptr->menu_item_ptr = NULL;
+    }
     free(submenu_ptr);
 }
 
 /* ------------------------------------------------------------------------- */
 wlmtk_menu_item_t *wlmtk_submenu_menu_item(wlmtk_submenu_t *submenu_ptr)
 {
-    return &submenu_ptr->super_menu_item;
+    return submenu_ptr->menu_item_ptr;
 }
 
 /* == Local (static) methods =============================================== */
 
 /* ------------------------------------------------------------------------- */
-void _wlmtk_submenu_element_destroy(wlmtk_element_t *element_ptr)
+/** Handles @ref wlmtk_menu_item_events_t::triggered. Triggers the action */
+void _wlmtk_submenu_handle_triggered(
+    struct wl_listener *listener_ptr,
+    __UNUSED__ void *data_ptr)
 {
     wlmtk_submenu_t *submenu_ptr = BS_CONTAINER_OF(
-        element_ptr, wlmtk_submenu_t,
-        super_menu_item.super_buffer.super_element);
-    wlmtk_submenu_destroy(submenu_ptr);
+        listener_ptr, wlmtk_submenu_t, triggered_listener);
+
+    bs_log(BS_ERROR, "FIXME: Clicked: %p", submenu_ptr);
 }
 
 /* ------------------------------------------------------------------------- */
-void _wlmtk_submenu_menu_item_clicked(wlmtk_menu_item_t *menu_item_ptr)
+/** Handles @ref wlmtk_menu_item_events_t::destroy. Destroy the action item. */
+void _wlmtk_submenu_handle_destroy(
+    struct wl_listener *listener_ptr,
+    __UNUSED__ void *data_ptr)
 {
     wlmtk_submenu_t *submenu_ptr = BS_CONTAINER_OF(
-        menu_item_ptr, wlmtk_submenu_t, super_menu_item);
+        listener_ptr, wlmtk_submenu_t, destroy_listener);
 
-    bs_log(BS_ERROR, "FIXME: Clicked: %p", submenu_ptr);
+    submenu_ptr->menu_item_ptr = NULL;
+    wlmtk_submenu_destroy(submenu_ptr);
 }
 
 /* == End of submenu.c ===================================================== */
