7cf7d24e348f62c3d081ea2df58302afc1da3c07
Philipp Kaeser <kaeser@gubbe.ch>
2025-02-23 11:45:23 +0100

    Shows or hides submenu, depending on item state.

--- src/toolkit/submenu.c
+++ src/toolkit/submenu.c
@@ -34,12 +34,17 @@ struct _wlmtk_submenu_t {
     wlmtk_menu_item_t         *item1_ptr;
     wlmtk_menu_item_t         *item2_ptr;
 
+    /** Listener for @ref wlmtk_menu_item_events_t::state_changed. */
+    struct wl_listener        state_changed_listener;
     /** Listener for @ref wlmtk_menu_item_events_t::triggered. */
     struct wl_listener        triggered_listener;
     /** Listener for @ref wlmtk_menu_item_events_t::destroy. */
     struct wl_listener        destroy_listener;
 };
 
+static void _wlmtk_submenu_handle_state_changed(
+    struct wl_listener *listener_ptr,
+    void *data_ptr);
 static void _wlmtk_submenu_handle_triggered(
     struct wl_listener *listener_ptr,
     void *data_ptr);
@@ -66,6 +71,10 @@ wlmtk_submenu_t *wlmtk_submenu_create(
         wlmtk_submenu_destroy(submenu_ptr);
         return NULL;
     }
+    wlmtk_util_connect_listener_signal(
+        &wlmtk_menu_item_events(submenu_ptr->menu_item_ptr)->state_changed,
+        &submenu_ptr->state_changed_listener,
+        _wlmtk_submenu_handle_state_changed);
     wlmtk_util_connect_listener_signal(
         &wlmtk_menu_item_events(submenu_ptr->menu_item_ptr)->triggered,
         &submenu_ptr->triggered_listener,
@@ -132,6 +141,7 @@ void wlmtk_submenu_destroy(wlmtk_submenu_t *submenu_ptr)
     if (NULL == submenu_ptr->menu_item_ptr) {
         wlmtk_util_disconnect_listener(&submenu_ptr->destroy_listener);
         wlmtk_util_disconnect_listener(&submenu_ptr->triggered_listener);
+        wlmtk_util_disconnect_listener(&submenu_ptr->state_changed_listener);
 
         wlmtk_menu_item_destroy(submenu_ptr->menu_item_ptr);
         submenu_ptr->menu_item_ptr = NULL;
@@ -146,6 +156,40 @@ wlmtk_menu_item_t *wlmtk_submenu_menu_item(wlmtk_submenu_t *submenu_ptr)
 }
 
 /* == Local (static) methods =============================================== */
+
+/* ------------------------------------------------------------------------- */
+/** Handle @ref wlmtk_menu_item_events_t::state_changed. Show/hide submenu. */
+void _wlmtk_submenu_handle_state_changed(
+    struct wl_listener *listener_ptr,
+    __UNUSED__ void *data_ptr)
+{
+    int x, y, t, r;
+    wlmtk_submenu_t *submenu_ptr = BS_CONTAINER_OF(
+        listener_ptr, wlmtk_submenu_t, state_changed_listener);
+    wlmtk_element_t *item_element_ptr = wlmtk_menu_item_element(
+        submenu_ptr->menu_item_ptr);
+    wlmtk_element_t *popup_element_ptr = wlmtk_popup_element(
+        wlmtk_popup_menu_popup(submenu_ptr->popup_menu_ptr));
+
+    switch (wlmtk_menu_item_get_state(submenu_ptr->menu_item_ptr)) {
+    case WLMTK_MENU_ITEM_HIGHLIGHTED:
+        wlmtk_element_get_position(item_element_ptr, &x, &y);
+        wlmtk_element_get_dimensions(item_element_ptr, NULL, &t, &r, NULL);
+        x += r;
+        y += t;
+        wlmtk_element_set_position(popup_element_ptr, x, y);
+        wlmtk_container_raise_element_to_top(
+            popup_element_ptr->parent_container_ptr, popup_element_ptr);
+        wlmtk_element_set_visible(popup_element_ptr, true);
+        break;
+
+    case WLMTK_MENU_ITEM_ENABLED:
+    case WLMTK_MENU_ITEM_DISABLED:
+    default:
+        wlmtk_element_set_visible(popup_element_ptr, false);
+        break;
+    }
+}
 
 /* ------------------------------------------------------------------------- */
 /** Handles @ref wlmtk_menu_item_events_t::triggered. Triggers the action */
