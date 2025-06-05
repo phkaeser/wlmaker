18633bd67d143e5b9dd84f591890d773c7839d28
Philipp Kaeser <kaeser@gubbe.ch>
2025-06-05 14:51:57 +0300

    toolkit: Updates locking to work correctly on multiple outputs.

--- src/toolkit/lock.c
+++ src/toolkit/lock.c
@@ -29,6 +29,7 @@
 #define WLR_USE_UNSTABLE
 #include <wlr/types/wlr_compositor.h>
 #include <wlr/types/wlr_output.h>
+#include <wlr/types/wlr_output_layout.h>
 #include <wlr/types/wlr_session_lock_v1.h>
 #undef WLR_USE_UNSTABLE
 #include "libbase/libbase.h"
@@ -52,8 +53,11 @@ struct _wlmtk_lock_t {
     /** The root this lock is applied for. */
     wlmtk_root_t              *root_ptr;
 
-    /** List of surfaces, via @ref wlmtk_lock_surface_t::dlnode. */
-    bs_dllist_t               lock_surfaces;
+    /** The output layout. */
+    struct wlr_output_layout  *wlr_output_layout_ptr;
+
+    /** Holds all @ref wlmtk_lock_surface_t, keyed by output. */
+    bs_avltree_t              *lock_surface_tree_ptr;
     /** Container holding the lock surfaces. */
     wlmtk_container_t         container;
 
@@ -63,6 +67,9 @@ struct _wlmtk_lock_t {
     struct wl_listener        unlock_listener;
     /** Listener for the `destroy` signal of `wlr_session_lock_v1`. */
     struct wl_listener        destroy_listener;
+
+    /** Event: Output layout changed. Parameter: struct wlr_box*. */
+    struct wl_listener        output_layout_change_listener;
 };
 
 /** State of a lock surface. */
@@ -74,8 +81,8 @@ struct _wlmtk_lock_surface_t {
     /** Back-link to the lock. */
     wlmtk_lock_t              *lock_ptr;
 
-    /** Link node, element of @ref wlmtk_lock_t::lock_surfaces. */
-    bs_dllist_node_t          dlnode;
+    /** Tree node, element of @ref wlmtk_lock_t::lock_surface_tree_ptr. */
+    bs_avltree_node_t         avlnode;
     /** Serial returned by `wlr_session_lock_surface_v1_configure`. */
     uint32_t                  configure_serial;
 
@@ -85,7 +92,15 @@ struct _wlmtk_lock_surface_t {
     struct wl_listener        surface_commit_listener;
 };
 
-static void _wlmtk_lock_report_surface_locked(
+/** Arguent to @ref _wlmtk_lock_update_output. */
+typedef struct {
+    /** The lock we're working on. */
+    wlmtk_lock_t              *lock_ptr;
+    /** The former lock surface tree. */
+    bs_avltree_t              *former_lock_surface_tree_ptr;
+} wlmtk_lock_update_output_arg_t;
+
+static void _wlmtk_lock_register_lock_surface(
     wlmtk_lock_t *lock_ptr,
     wlmtk_lock_surface_t *lock_surface_ptr);
 
@@ -98,17 +113,18 @@ static void _wlmtk_lock_handle_unlock(
 static void _wlmtk_lock_handle_destroy(
     struct wl_listener *listener_ptr,
     void *data_ptr);
+static void _wlmtk_lock_handle_output_layout_change(
+    struct wl_listener *listener_ptr,
+    void *data_ptr);
+static bool _wlmtk_lock_update_output(
+    struct wl_list *link_ptr,
+    void *ud_ptr);
 
 static wlmtk_lock_surface_t *_wlmtk_lock_surface_create(
     struct wlr_session_lock_surface_v1 *wlr_session_lock_surface_v1_ptr,
     wlmtk_lock_t *lock_ptr);
 static void _wlmtk_lock_surface_destroy(
     wlmtk_lock_surface_t *lock_surface_ptr);
-static void _wlmtk_lock_surface_dlnode_destroy(
-    bs_dllist_node_t *dlnode_ptr, void *ud_ptr);
-static bool _wlmtk_lock_surface_has_wlr_output(
-    bs_dllist_node_t *dlnode_ptr,
-    void *ud_ptr);
 
 static void _wlmtk_lock_surface_handle_destroy(
     struct wl_listener *listener_ptr,
@@ -117,24 +133,42 @@ static void _wlmtk_lock_surface_handle_surface_commit(
     struct wl_listener *listener_ptr,
     void *data_ptr);
 
+static int _wlmtk_lock_surface_avlnode_cmp(
+    const bs_avltree_node_t *node_ptr,
+    const void *key_ptr);
+static void _wlmtk_lock_surface_avlnode_destroy(
+    bs_avltree_node_t *node_ptr);
+
 /* == Exported methods ===================================================== */
 
 /* ------------------------------------------------------------------------- */
 wlmtk_lock_t *wlmtk_lock_create(
     struct wlr_session_lock_v1 *wlr_session_lock_v1_ptr,
+    struct wlr_output_layout *wlr_output_layout_ptr,
     wlmtk_root_t *root_ptr,
     wlmtk_env_t *env_ptr)
 {
     wlmtk_lock_t *lock_ptr = logged_calloc(1, sizeof(wlmtk_lock_t));
     if (NULL == lock_ptr) return NULL;
     lock_ptr->wlr_session_lock_v1_ptr = wlr_session_lock_v1_ptr;
+    lock_ptr->wlr_output_layout_ptr = wlr_output_layout_ptr;
     lock_ptr->root_ptr = root_ptr;
+
+    lock_ptr->lock_surface_tree_ptr = bs_avltree_create(
+        _wlmtk_lock_surface_avlnode_cmp,
+        _wlmtk_lock_surface_avlnode_destroy);
+    if (NULL == lock_ptr->lock_surface_tree_ptr) {
+        bs_log(BS_ERROR, "Failed bs_avltree_create(%p, %p)",
+               _wlmtk_lock_surface_avlnode_cmp,
+               _wlmtk_lock_surface_avlnode_destroy);
+        wlmtk_lock_destroy(lock_ptr);
+        return NULL;
+    }
 
     if (!wlmtk_container_init(&lock_ptr->container, env_ptr)) {
         wlmtk_lock_destroy(lock_ptr);
         return NULL;
     }
-    wlmtk_element_set_visible(&lock_ptr->container.super_element, true);
 
     wlmtk_util_connect_listener_signal(
         &lock_ptr->wlr_session_lock_v1_ptr->events.new_surface,
@@ -149,14 +183,27 @@ wlmtk_lock_t *wlmtk_lock_create(
         &lock_ptr->destroy_listener,
         _wlmtk_lock_handle_destroy);
 
+    wlmtk_util_connect_listener_signal(
+        &wlr_output_layout_ptr->events.change,
+        &lock_ptr->output_layout_change_listener,
+        _wlmtk_lock_handle_output_layout_change);
+    _wlmtk_lock_handle_output_layout_change(
+        &lock_ptr->output_layout_change_listener,
+        NULL);
+
     return lock_ptr;
 }
 
 /* ------------------------------------------------------------------------- */
 void wlmtk_lock_destroy(wlmtk_lock_t *lock_ptr)
 {
-    bs_dllist_for_each(
-        &lock_ptr->lock_surfaces, _wlmtk_lock_surface_dlnode_destroy, NULL);
+    wlmtk_util_disconnect_listener(
+        &lock_ptr->output_layout_change_listener);
+
+    if (NULL != lock_ptr->lock_surface_tree_ptr) {
+        bs_avltree_destroy(lock_ptr->lock_surface_tree_ptr);
+        lock_ptr->lock_surface_tree_ptr = NULL;
+    }
 
     wl_list_remove(&lock_ptr->destroy_listener.link);
     wl_list_remove(&lock_ptr->unlock_listener.link);
@@ -179,36 +226,45 @@ wlmtk_element_t *wlmtk_lock_element(wlmtk_lock_t *lock_ptr)
 
 /* ------------------------------------------------------------------------- */
 /**
- * Registers the provided surface as 'locked'. Locks the session, if all
- * outputs have been locked.
+ * Registers the provided surface as a lock surface for it's output, unless
+ * it had already been registered. Locks the session, once lock surfaces for
+ * all surfaces have been registered.
  *
  * @param lock_ptr
  * @param lock_surface_ptr
  */
-void _wlmtk_lock_report_surface_locked(
+void _wlmtk_lock_register_lock_surface(
     wlmtk_lock_t *lock_ptr,
     wlmtk_lock_surface_t *lock_surface_ptr)
 {
-    // Guard clause: Don't add the surface if already reported.
-    if (bs_dllist_contains(
-            &lock_ptr->lock_surfaces, &lock_surface_ptr->dlnode)) return;
-
-    // Another guard: Don't accept the same output twice.
-    if (bs_dllist_find(
-            &lock_ptr->lock_surfaces,
-            _wlmtk_lock_surface_has_wlr_output,
-            lock_surface_ptr->wlr_session_lock_surface_v1_ptr->output)) {
-        bs_log(BS_WARNING, "Extra lock surface detected for wlr_output %p",
-               lock_surface_ptr->wlr_session_lock_surface_v1_ptr->output);
-        wl_resource_post_error(
-            lock_surface_ptr->wlr_session_lock_surface_v1_ptr->resource,
-            WL_DISPLAY_ERROR_INVALID_METHOD,
-            "Extra lock surface detected for wlr_output %p",
-            lock_surface_ptr->wlr_session_lock_surface_v1_ptr->output);
+    // Guard clause: Don't register more than once.
+    bs_avltree_node_t *avlnode_ptr = bs_avltree_lookup(
+        lock_ptr->lock_surface_tree_ptr,
+        lock_surface_ptr->wlr_session_lock_surface_v1_ptr->output);
+    if (NULL != avlnode_ptr) {
+        wlmtk_lock_surface_t *existing_lock_surface_ptr = BS_CONTAINER_OF(
+            avlnode_ptr, wlmtk_lock_surface_t, avlnode);
+        if (existing_lock_surface_ptr != lock_surface_ptr) {
+            // Registering more than one surfaces per output is a protocol
+            // violation. wlroots is supposed to catch these, so this is just
+            // an extra safety check.
+            wl_resource_post_error(
+                lock_ptr->wlr_session_lock_v1_ptr->resource,
+                WL_DISPLAY_ERROR_INVALID_METHOD,
+                "Extra lock surface %p found for wlr_output %p (surface %p)",
+                lock_surface_ptr->wlr_session_lock_surface_v1_ptr,
+                lock_surface_ptr->wlr_session_lock_surface_v1_ptr->output,
+                existing_lock_surface_ptr->wlr_session_lock_surface_v1_ptr);
+        }
         return;
     }
 
-    bs_dllist_push_back(&lock_ptr->lock_surfaces, &lock_surface_ptr->dlnode);
+    // Inserting *must* succeed -- we verified above that there's no duplicate.
+    BS_ASSERT(bs_avltree_insert(
+                  lock_ptr->lock_surface_tree_ptr,
+                  lock_surface_ptr->wlr_session_lock_surface_v1_ptr->output,
+                  &lock_surface_ptr->avlnode,
+                  false));
     wlmtk_container_add_element(
         &lock_ptr->container,
         wlmtk_surface_element(lock_surface_ptr->wlmtk_surface_ptr));
@@ -216,22 +272,22 @@ void _wlmtk_lock_report_surface_locked(
         wlmtk_surface_element(lock_surface_ptr->wlmtk_surface_ptr), true);
 
     // If not all outputs are covered: No lock yet.
-    // FIXME
-    /* if (bs_dllist_size(&lock_ptr->lock_surfaces) < */
-    /*     bs_dllist_size(&lock_ptr->lock_mgr_ptr->server_ptr->outputs)) return; */
+    if (bs_avltree_size(lock_ptr->lock_surface_tree_ptr) <
+        (size_t)wl_list_length(&lock_ptr->wlr_output_layout_ptr->outputs)) {
+        return;
+    }
 
     if (!wlmtk_root_lock(lock_ptr->root_ptr, wlmtk_lock_element(lock_ptr))) {
         wl_resource_post_error(
-            lock_surface_ptr->wlr_session_lock_surface_v1_ptr->resource,
+            lock_ptr->wlr_session_lock_v1_ptr->resource,
             WL_DISPLAY_ERROR_INVALID_METHOD,
             "Failed wlmtk_root_lock(%p, %p): Already locked?",
             lock_ptr->root_ptr, wlmtk_lock_element(lock_ptr));
         return;
     }
-    // Grant keyboard focus to the lock's surface.
-    wlmtk_lock_surface_t *first_surface_ptr = BS_CONTAINER_OF(
-        lock_ptr->lock_surfaces.head_ptr, wlmtk_lock_surface_t, dlnode);
-    wlmtk_surface_set_activated(first_surface_ptr->wlmtk_surface_ptr, true);
+    wlmtk_element_set_visible(&lock_ptr->container.super_element, true);
+    // Grant keyboard focus to the lock surface we just added.
+    wlmtk_surface_set_activated(lock_surface_ptr->wlmtk_surface_ptr, true);
 
     // Root is locked. Send confirmation to the client.
     wlr_session_lock_v1_send_locked(lock_ptr->wlr_session_lock_v1_ptr);
@@ -285,6 +341,7 @@ void _wlmtk_lock_handle_unlock(
     __UNUSED__ wlmtk_lock_t *lock_ptr = BS_CONTAINER_OF(
         listener_ptr, wlmtk_lock_t, unlock_listener);
 
+    wlmtk_element_set_visible(&lock_ptr->container.super_element, false);
     wlmtk_root_unlock(lock_ptr->root_ptr, wlmtk_lock_element(lock_ptr));
 }
 
@@ -302,6 +359,89 @@ void _wlmtk_lock_handle_destroy(
     wlmtk_lock_t *lock_ptr = BS_CONTAINER_OF(
         listener_ptr, wlmtk_lock_t, destroy_listener);
     wlmtk_lock_destroy(lock_ptr);
+}
+
+/* ------------------------------------------------------------------------- */
+/**
+ * Handles the `change` event of wlr_output_layout::events.
+ *
+ * Walks through outputs of @ref wlmtk_lock_t::wlr_output_layout_ptr, and
+ * updates @ref wlmtk_lock_t::lock_surface_tree_ptr accordingly.
+ *
+ * @param listener_ptr
+ * @param data_ptr
+ */
+void _wlmtk_lock_handle_output_layout_change(
+    struct wl_listener *listener_ptr,
+    __UNUSED__ void *data_ptr)
+{
+    wlmtk_lock_t *lock_ptr = BS_CONTAINER_OF(
+        listener_ptr, wlmtk_lock_t, output_layout_change_listener);
+
+    wlmtk_lock_update_output_arg_t arg = {
+        .lock_ptr = lock_ptr,
+        .former_lock_surface_tree_ptr = lock_ptr->lock_surface_tree_ptr
+    };
+
+    lock_ptr->lock_surface_tree_ptr = bs_avltree_create(
+        _wlmtk_lock_surface_avlnode_cmp,
+        _wlmtk_lock_surface_avlnode_destroy);
+    BS_ASSERT(NULL != lock_ptr->lock_surface_tree_ptr);
+
+    BS_ASSERT(wlmtk_util_wl_list_for_each(
+                  &lock_ptr->wlr_output_layout_ptr->outputs,
+                  _wlmtk_lock_update_output,
+                  &arg));
+
+    bs_avltree_destroy(arg.former_lock_surface_tree_ptr);
+}
+
+/* ------------------------------------------------------------------------- */
+/**
+ * Updates the lock surfaces for an output: If there is a surface for the
+ * output, moves it into the new surface tree, and sends a configure() event
+ * with new dimensions. Otherwise, a no-op.
+ *
+ * @param link_ptr
+ * @param ud_ptr              A @ref wlmtk_lock_update_output_arg_t.
+ *
+ * @return true if the operation succeeded. It may fail if there's multiple
+ * surface per output; although that would be indicative of data corruption.
+ */
+bool _wlmtk_lock_update_output(
+    struct wl_list *link_ptr,
+    void *ud_ptr)
+{
+    struct wlr_output_layout_output *wlr_output_layout_output_ptr =
+        BS_CONTAINER_OF(link_ptr, struct wlr_output_layout_output, link);
+    struct wlr_output *wlr_output_ptr = wlr_output_layout_output_ptr->output;
+    wlmtk_lock_update_output_arg_t *arg_ptr = ud_ptr;
+
+    bs_avltree_node_t *avlnode_ptr = bs_avltree_delete(
+        arg_ptr->former_lock_surface_tree_ptr, wlr_output_ptr);
+    if (NULL == avlnode_ptr) {
+        // We have a new output, but no lock surface. That can happen. The
+        // curtain will have to cover it.
+        return true;
+    }
+
+    wlmtk_lock_surface_t *lock_surface_ptr = BS_CONTAINER_OF(
+        avlnode_ptr, wlmtk_lock_surface_t, avlnode);
+    BS_ASSERT(
+        wlr_output_ptr =
+        lock_surface_ptr->wlr_session_lock_surface_v1_ptr->output);
+
+    // The output dimensions may have changed. Send a configure().
+    wlr_session_lock_surface_v1_configure(
+        lock_surface_ptr->wlr_session_lock_surface_v1_ptr,
+        lock_surface_ptr->wlr_session_lock_surface_v1_ptr->output->width,
+        lock_surface_ptr->wlr_session_lock_surface_v1_ptr->output->height);
+
+    return bs_avltree_insert(
+        arg_ptr->lock_ptr->lock_surface_tree_ptr,
+        wlr_output_ptr,
+        &lock_surface_ptr->avlnode,
+        false);
 }
 
 /* ------------------------------------------------------------------------- */
@@ -371,10 +511,13 @@ wlmtk_lock_surface_t *_wlmtk_lock_surface_create(
  */
 void _wlmtk_lock_surface_destroy(wlmtk_lock_surface_t *lock_surface_ptr)
 {
-    if (bs_dllist_contains(&lock_surface_ptr->lock_ptr->lock_surfaces,
-                           &lock_surface_ptr->dlnode)) {
-        bs_dllist_remove(&lock_surface_ptr->lock_ptr->lock_surfaces,
-                         &lock_surface_ptr->dlnode);
+    bs_log(BS_INFO, "Lock %p: Destroying lock surface %p",
+           lock_surface_ptr->lock_ptr, lock_surface_ptr);
+
+    if (NULL != bs_avltree_delete(
+            lock_surface_ptr->lock_ptr->lock_surface_tree_ptr,
+            lock_surface_ptr->wlr_session_lock_surface_v1_ptr->output)) {
+        // Means: This lock surface had been registered.
         wlmtk_container_remove_element(
             &lock_surface_ptr->lock_ptr->container,
             wlmtk_surface_element(lock_surface_ptr->wlmtk_surface_ptr));
@@ -389,27 +532,6 @@ void _wlmtk_lock_surface_destroy(wlmtk_lock_surface_t *lock_surface_ptr)
     }
 
     free(lock_surface_ptr);
-}
-
-/* ------------------------------------------------------------------------- */
-/** Callback for `bs_dllist_for_each` to destroy all lock surfaces. */
-void _wlmtk_lock_surface_dlnode_destroy(
-    bs_dllist_node_t *dlnode_ptr, __UNUSED__ void *ud_ptr)
-{
-    wlmtk_lock_surface_t *lock_surface_ptr = BS_CONTAINER_OF(
-        dlnode_ptr, wlmtk_lock_surface_t, dlnode);
-    _wlmtk_lock_surface_destroy(lock_surface_ptr);
-}
-
-/* ------------------------------------------------------------------------- */
-/** Returns whether the surface at dlnode_ptr has wlr_output == ud_ptr. */
-bool _wlmtk_lock_surface_has_wlr_output(
-    bs_dllist_node_t *dlnode_ptr,
-    void *ud_ptr)
-{
-    wlmtk_lock_surface_t *lock_surface_ptr = BS_CONTAINER_OF(
-        dlnode_ptr, wlmtk_lock_surface_t, dlnode);
-    return lock_surface_ptr->wlr_session_lock_surface_v1_ptr->output == ud_ptr;
 }
 
 /* ------------------------------------------------------------------------- */
@@ -453,9 +575,31 @@ void _wlmtk_lock_surface_handle_surface_commit(
     // Do not accept locking for commits before the requested configuration.
     if (wlr_session_lock_surface_v1_ptr->current.configure_serial >=
         lock_surface_ptr->configure_serial) {
-        _wlmtk_lock_report_surface_locked(
+        _wlmtk_lock_register_lock_surface(
             lock_surface_ptr->lock_ptr, lock_surface_ptr);
     }
+}
+
+/* ------------------------------------------------------------------------- */
+/** Comparator for @ref wlmtk_lock_t::lock_surface_tree_ptr by output. */
+int _wlmtk_lock_surface_avlnode_cmp(
+    const bs_avltree_node_t *avlnode_ptr,
+    const void *key_ptr)
+{
+    wlmtk_lock_surface_t *lock_surface_ptr = BS_CONTAINER_OF(
+        avlnode_ptr, wlmtk_lock_surface_t, avlnode);
+    return bs_avltree_cmp_ptr(
+        lock_surface_ptr->wlr_session_lock_surface_v1_ptr->output, key_ptr);
+}
+
+/* ------------------------------------------------------------------------- */
+/** Dtor for  @ref wlmtk_lock_t::lock_surface_tree_ptr nodes. */
+void _wlmtk_lock_surface_avlnode_destroy(
+    bs_avltree_node_t *avlnode_ptr)
+{
+    wlmtk_lock_surface_t *lock_surface_ptr = BS_CONTAINER_OF(
+        avlnode_ptr, wlmtk_lock_surface_t, avlnode);
+    _wlmtk_lock_surface_destroy(lock_surface_ptr);
 }
 
 /* == End of lock.c ======================================================== */
