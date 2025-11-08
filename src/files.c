91621fc79c52ebe68729780d48e14a3718a62d63
Philipp Kaeser <kaeser@gubbe.ch>
2025-11-08 10:25:28 +0100

    files: Adds module for constructing filenames following XDG base directory specification.

---
+++ src/files.c
@@ -0,0 +1,110 @@
+/* ========================================================================= */
+/**
+ * @file files.c
+ *
+ * @copyright
+ * Copyright 2025 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include "files.h"
+
+#include <stdlib.h>
+#include <basedir.h>
+#include <libbase/libbase.h>
+
+/* == Declarations ========================================================= */
+
+/** State of the files module. */
+struct _wlmaker_files_t {
+    /** Handle for libxdg-basedir. */
+    xdgHandle                 xdg_handle;
+
+    /** Directory name to use beneath the XDG_<any>_HOME paths. */
+    char                      *dirname_ptr;
+};
+
+/* == Exported methods ===================================================== */
+
+/* ------------------------------------------------------------------------- */
+wlmaker_files_t *wlmaker_files_create(const char *dirname_ptr)
+{
+    wlmaker_files_t *files_ptr = logged_calloc(1, sizeof(wlmaker_files_t));
+    if (NULL == files_ptr) return NULL;
+
+    files_ptr->dirname_ptr = logged_strdup(dirname_ptr);
+    if (NULL == files_ptr->dirname_ptr) goto error;
+
+    if (NULL == xdgInitHandle(&files_ptr->xdg_handle)) goto error;
+
+    return files_ptr;
+
+error:
+    wlmaker_files_destroy(files_ptr);
+    return NULL;
+
+}
+
+/* ------------------------------------------------------------------------- */
+void wlmaker_files_destroy(wlmaker_files_t *files_ptr)
+{
+    if (NULL != files_ptr->dirname_ptr) {
+        free(files_ptr->dirname_ptr);
+        files_ptr->dirname_ptr = NULL;
+    }
+
+    xdgWipeHandle(&files_ptr->xdg_handle);
+    free(files_ptr);
+}
+
+/* ------------------------------------------------------------------------- */
+char *wlmaker_files_xdg_config_fname(
+    wlmaker_files_t *files_ptr,
+    const char *fname_ptr)
+{
+    const char *config_home_ptr = xdgConfigHome(&files_ptr->xdg_handle);
+    if (NULL == config_home_ptr) return NULL;
+
+    return bs_strdupf(
+        "%s/%s/%s",
+        config_home_ptr,
+        files_ptr->dirname_ptr,
+        fname_ptr);
+}
+
+/* == Unit Tests =========================================================== */
+
+static void _wlmaker_files_test_builders(bs_test_t *test_ptr);
+
+const bs_test_case_t wlmaker_files_test_cases[] = {
+    { 1, "builders", _wlmaker_files_test_builders },
+    { 0, NULL, NULL }
+};
+
+/* ------------------------------------------------------------------------- */
+/** Tests building filenames relative to XDG base directories. */
+void _wlmaker_files_test_builders(bs_test_t *test_ptr)
+{
+    wlmaker_files_t *files_ptr = wlmaker_files_create("wlmaker");
+    BS_TEST_VERIFY_NEQ_OR_RETURN(test_ptr, NULL, files_ptr);
+
+    char *f = wlmaker_files_xdg_config_fname(files_ptr, "state.plist");
+    BS_TEST_VERIFY_NEQ_OR_RETURN(test_ptr, NULL, f);
+    BS_TEST_VERIFY_STRMATCH(test_ptr, f, "/wlmaker/state.plist$");
+    free(f);
+
+    wlmaker_files_destroy(files_ptr);
+}
+
+/* == End of files.c ======================================================= */
---
+++ src/files.h
@@ -0,0 +1,72 @@
+/* ========================================================================= */
+/**
+ * @file files.h
+ *
+ * @copyright
+ * Copyright 2025 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+#ifndef __WLMAKER_FILES_H__
+#define __WLMAKER_FILES_H__
+
+#include <libbase/libbase.h>
+
+/** Handle for files module. */
+typedef struct _wlmaker_files_t wlmaker_files_t;
+
+#ifdef __cplusplus
+extern "C" {
+#endif  // __cplusplus
+
+/**
+ * Create the files module.
+ *
+ * @param dirname_ptr         Name of a subdirectory, that will be used as a
+ *                            component for each of the XDG paths.
+ *
+ * @return Pointer to the module handle. Must be freed by calling
+ *     @ref wlmaker_files_destroy.
+ */
+wlmaker_files_t *wlmaker_files_create(const char *dirname_ptr);
+
+/**
+ * Destroys the file module.
+ *
+ * @param files_ptr
+ */
+void wlmaker_files_destroy(wlmaker_files_t *files_ptr);
+
+/**
+ * Returns a full path name for a config file.
+ *
+ * This expands into ${XDG_CONFIG_HOME}/`*dirname_ptr`/`*fname_ptr`.
+ *
+ * @param files_ptr
+ * @param fname_ptr
+ *
+ * @return Full path name, or NULL on error.
+ */
+char *wlmaker_files_xdg_config_fname(
+    wlmaker_files_t *files_ptr,
+    const char *fname_ptr);
+
+/** Unit test cases for @ref wlmaker_files_t. */
+extern const bs_test_case_t wlmaker_files_test_cases[];
+
+#ifdef __cplusplus
+}  // extern "C"
+#endif  // __cplusplus
+
+#endif /* __WLMAKER_FILES_H__ */
+/* == End of files.h ======================================================= */
