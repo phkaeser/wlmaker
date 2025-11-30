6b58cf8271303077620b8807fc7a941e201b4fff
Philipp Kaeser <kaeser@gubbe.ch>
2025-11-30 13:51:01 +0100

    build: Removes conditional build for wlroots 0.17. It had already been removed from CMakeLists.txt.

--- src/toolkit/titlebar_title.c
+++ src/toolkit/titlebar_title.c
@@ -31,7 +31,6 @@
 #include <wlr/interfaces/wlr_buffer.h>
 #include <wlr/types/wlr_output.h>
 #include <wlr/types/wlr_output_layout.h>
-#include <wlr/version.h>
 #undef WLR_USE_UNSTABLE
 
 #include "buffer.h"
@@ -232,16 +231,10 @@ bool _wlmtk_titlebar_title_element_pointer_axis(
 
     // Only consider vertical wheel moves.
     if (
-#if WLR_VERSION_NUM >= (18 << 8)
         (WL_POINTER_AXIS_SOURCE_WHEEL != wlr_pointer_axis_event_ptr->source &&
          WL_POINTER_AXIS_SOURCE_FINGER != wlr_pointer_axis_event_ptr->source) ||
         WL_POINTER_AXIS_VERTICAL_SCROLL !=
         wlr_pointer_axis_event_ptr->orientation
-#else // WLR_VERSION_NUM >= (18 << 8)
-        (WLR_AXIS_SOURCE_WHEEL != wlr_pointer_axis_event_ptr->source &&
-         WLR_AXIS_SOURCE_FINGER != wlr_pointer_axis_event_ptr->source) ||
-        WLR_AXIS_ORIENTATION_VERTICAL !=wlr_pointer_axis_event_ptr->orientation
-#endif // WLR_VERSION_NUM >= (18 << 8)
         ) {
         return false;
     }
@@ -463,13 +456,8 @@ void test_shade(bs_test_t *test_ptr)
     BS_TEST_VERIFY_FALSE(test_ptr, wlmtk_window_is_shaded(w));
 
     struct wlr_pointer_axis_event axis_event = {
-#if WLR_VERSION_NUM >= (18 << 8)
         .source = WL_POINTER_AXIS_SOURCE_WHEEL,
         .orientation = WL_POINTER_AXIS_VERTICAL_SCROLL,
-#else // WLR_VERSION_NUM >= (18 << 8)
-        .source = WLR_AXIS_SOURCE_WHEEL,
-        .orientation = WLR_AXIS_ORIENTATION_VERTICAL,
-#endif // WLR_VERSION_NUM >= (18 << 8)
         .delta = -0.01
     };
 
@@ -488,11 +476,7 @@ void test_shade(bs_test_t *test_ptr)
     BS_TEST_VERIFY_FALSE(test_ptr, wlmtk_window_is_shaded(w));
 
     // Source 'finger from a touchpad' is accepted, too.
-#if WLR_VERSION_NUM >= (18 << 8)
     axis_event.source = WL_POINTER_AXIS_SOURCE_FINGER;
-#else // WLR_VERSION_NUM >= (18 << 8)
-    axis_event.source = WLR_AXIS_SOURCE_FINGER;
-#endif // WLR_VERSION_NUM >= (18 << 8)
     axis_event.delta = -0.01;
     wlmtk_element_pointer_axis(element_ptr, &axis_event);
     BS_TEST_VERIFY_TRUE(test_ptr, wlmtk_window_is_shaded(w));
@@ -502,11 +486,7 @@ void test_shade(bs_test_t *test_ptr)
     BS_TEST_VERIFY_FALSE(test_ptr, wlmtk_window_is_shaded(w));
 
     // Axis from another source: Ignored.
-#if WLR_VERSION_NUM >= (18 << 8)
     axis_event.source = WL_POINTER_AXIS_SOURCE_WHEEL_TILT;
-#else // WLR_VERSION_NUM >= (18 << 8)
-    axis_event.source = WLR_AXIS_SOURCE_WHEEL_TILT;
-#endif // WLR_VERSION_NUM >= (18 << 8)
     axis_event.delta = -0.01;
     wlmtk_element_pointer_axis(element_ptr, &axis_event);
     BS_TEST_VERIFY_FALSE(test_ptr, wlmtk_window_is_shaded(w));
