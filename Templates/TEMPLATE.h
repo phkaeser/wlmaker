52e8ec5d7087b38c4e4ab182a043fb010d0458be
Philipp Kaeser <kaeser@gubbe.ch>
2025-12-28 14:37:46 +0100

    build: Updates emacs templates to conform to yasnippets syntax.

--- Templates/TEMPLATE.h
+++ Templates/TEMPLATE.h
@@ -1,9 +1,9 @@
 /* ========================================================================= */
 /**
- * @file (>>>FILE<<<)
+ * @file `(file-name-nondirectory (buffer-file-name))`
  *
  * @copyright
- * Copyright (>>>YEAR<<<) Google LLC
+ * Copyright (c) `(format-time-string "%Y")` Google LLC and `user-full-name`
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -17,18 +17,18 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-#ifndef __WLMAKER_(>>>FILE_UPCASE<<<)_H__
-#define __WLMAKER_(>>>FILE_UPCASE<<<)_H__
+#ifndef __WLMAKER_`(upcase (file-name-nondirectory (file-name-sans-extension (buffer-file-name))))`_H__
+#define __WLMAKER_`(upcase (file-name-nondirectory (file-name-sans-extension (buffer-file-name))))`_H__
 
 #ifdef __cplusplus
 extern "C" {
 #endif  // __cplusplus
 
-(>>>POINT<<<)
+$0
 
 #ifdef __cplusplus
 }  // extern "C"
 #endif  // __cplusplus
 
-#endif /* __WLMAKER_(>>>FILE_UPCASE<<<)_H__ */
-/* == End of (>>>FILE<<<) ================================================== */
+#endif __WLMAKER_`(upcase (file-name-nondirectory (file-name-sans-extension (buffer-file-name))))`_H__
+/* == End of `(file-name-nondirectory (buffer-file-name))` ================= */
