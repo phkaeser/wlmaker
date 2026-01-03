0a211bdab7ef445a460d459a82933d6005193d8b
Philipp Kaeser <kaeser@gubbe.ch>
2026-01-03 12:52:43 +0100

    FIXME.

---
+++ tool/wlmtool.c
@@ -0,0 +1,32 @@
+/* ========================================================================= */
+/**
+ * @file wlmtool.c
+ *
+ * Command-line tool for Wayland Maker.
+ *
+ * @copyright
+ * Copyright (c) 2026 Google LLC and Philipp Kaeser
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include <libbase/libbase.h>
+#include <stdlib.h>
+
+/* == Main program ========================================================= */
+int main(__UNUSED__ int argc, __UNUSED__ char *argv[])
+{
+    return EXIT_SUCCESS;
+}
+
+/* == End of wlmtool.c ===================================================== */
