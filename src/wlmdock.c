3828e2024a3da15a66c660bc7931c6a7a1479120
Philipp Kaeser <kaeser@gubbe.ch>
2026-08-21 15:24:24 +0200

    build cleanup: Moves wlmdock.c into src/ and makes library names consistent.

--- src/wlmdock.c
+++ src/wlmdock.c
@@ -55,9 +55,9 @@
 #include "wlclient/layer_surface.h"
 #include "wlclient/wlclient.h"
 
-#include "launcher.h"
-#include "tilebox.h"
-#include "subcompositor.h"
+#include "dock/launcher.h"
+#include "dock/tilebox.h"
+#include "dock/subcompositor.h"
 
 // TODO(kaeser@gubbe.ch): Move into a shared directory.
 #include "../config.h"
